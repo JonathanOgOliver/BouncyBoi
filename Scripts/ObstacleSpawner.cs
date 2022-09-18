7383eed26d9592c99dc1d4125b2241394c7098c0
Oliver Vejen <olivervejen47@gmail.com>
2022-09-18 03:17:23 +0200

    sååå da random trampolinnos ;)

--- Scripts/ObstacleSpawner.cs
+++ Scripts/ObstacleSpawner.cs
@@ -5,9 +5,10 @@ using System.Collections.Generic;
 public class ObstacleSpawner : Node2D
 {
     [Export] PackedScene ObstacleScene;
-    [Export] float minTimeInterval = 0;
-    [Export] float maxTimeInterval = 1;
-    [Export] float placementOffset = 100;
+    [Export] float minDistanceInterval = 100;
+    [Export] float maxDistanceInterval = 2000;
+    [Export] float nextInterval;
+    [Export] float placementOffset = 1000;
     [Export] float killDistance = 100;
     private Queue<Node2D> objects = new Queue<Node2D>();
     
@@ -24,6 +25,7 @@ public class ObstacleSpawner : Node2D
             return farthestObject;
         } 
     }
+    private float RandomInterval => (float)GD.RandRange(minDistanceInterval, maxDistanceInterval);
 
 
     public override void _Ready()
@@ -36,37 +38,34 @@ public class ObstacleSpawner : Node2D
         if(IsInstanceValid(BouncyBoy.Current) ){
             if(!isStarted){
                 isStarted = true;
-                float time = (float)GD.RandRange(minTimeInterval, maxTimeInterval);
-                GD.Print(time);
-                GetTree().CreateTimer(time, false).Connect("timeout", this, "TimerCallback");
+                nextInterval = BouncyBoy.Current.GlobalPosition.x + RandomInterval;
             }
-            if(GetFarthestObject != null && (Position-BouncyBoy.Current.Position).Length() > killDistance){
+            if(BouncyBoy.Current.GlobalPosition.x >= nextInterval){
+                SpawnObstacle();
+                nextInterval = BouncyBoy.Current.GlobalPosition.x + RandomInterval;
+            }
+            if(GetFarthestObject != null && BouncyBoy.Current.GlobalPosition.x-killDistance >= GetFarthestObject.GlobalPosition.x){
                 GetFarthestObject.QueueFree();
+                farthestObject = null;
             }
+        }else{
+            objects.Clear();
         }
 
     }
 
-    public void TimerCallback(){
-        if(IsInstanceValid(BouncyBoy.Current)){
-            GD.Randomize();
+    private void SpawnObstacle(){
+        GD.Randomize();
 
-            float xPos = (BouncyBoy.Current.Camera.Position.x+((GetViewportRect().Size.x*BouncyBoy.Current.Camera.Zoom.x)/2f))+placementOffset;
-            float yPos = (float)GD.RandRange(2, GetViewportRect().Size.y*BouncyBoy.Current.Camera.Zoom.y);
-            float rot = (float)GD.RandRange(0, 360);
+        float xPos = (BouncyBoy.Current.Camera.GlobalPosition.x+((GetViewportRect().Size.x*BouncyBoy.Current.Camera.Zoom.x)/2f))+placementOffset;
+        float yPos = (float)GD.RandRange(2, -GetViewportRect().Size.y*BouncyBoy.Current.Camera.Zoom.y);
+        float rot = (float)GD.RandRange(0, 360);
 
-            Node2D newBoi = ObstacleScene.Instance<Node2D>();
-            newBoi.Position = new Vector2(xPos, yPos);
-            newBoi.RotationDegrees = rot;
+        Node2D newBoi = ObstacleScene.Instance<Node2D>();
+        newBoi.GlobalPosition = new Vector2(xPos, yPos);
+        newBoi.RotationDegrees = rot;
 
-            AddChild(newBoi);
-            
-            float time = (float)GD.RandRange(minTimeInterval, maxTimeInterval);
-            GD.Print(time);
-
-            GetTree().CreateTimer(time, false).Connect("timeout", this, "TimerCallback");
-        }else{
-            objects.Clear();
-        }
+        AddChild(newBoi);
+        objects.Enqueue(newBoi);
     }
 }
